ef43de42f8b712eed7b1f848465f111f39a3c19c
Marc Stevenson <marcsstevenson@hotmail.com>
2016-04-18 13:22:47 +1200

    progress

--- app/CodeWeaveController.js
+++ app/CodeWeaveController.js
@@ -13,12 +13,14 @@
                 $localStorage.Take = $scope.Take;
                 $localStorage.WeaveSubstitution = $scope.WeaveSubstitution;
                 $localStorage.WeaveValues = $scope.WeaveValues;
+                $localStorage.FilterIn = $scope.FilterIn;
             }
             
             self.Initialise = function(){
                 $scope.Take = $localStorage.Take ? $localStorage.Take : '';
                 $scope.WeaveSubstitution = $localStorage.WeaveSubstitution ? $localStorage.WeaveSubstitution : '';
-                $scope.WeaveValues = $localStorage.WeaveValues ? $localStorage.WeaveValues : '';
+                $scope.WeaveValues = $localStorage.WeaveValues ? $localStorage.WeaveValues : '';                
+                $scope.FilterIn = $localStorage.FilterIn ? $localStorage.FilterIn : '';
                 
                 var delayInMs = 2000;
 
@@ -37,6 +39,7 @@
                 self.Initialised = true;
                 
                 $scope.Weave();
+                $scope.Filter();
             }
             
             $scope.Weave = function(){
@@ -60,6 +63,55 @@
 
                 return str.replace(new RegExp(find, 'g'), replace);
             }
+            
+            $scope.Filter = function(){
+                if(!self.Initialised) return;
+                if($scope.FilterIn.length === 0) return;
+                
+                self.SaveToStorage();
+                
+                var filterIn = $scope.FilterIn;
+                var filterOut = $scope.FilterIn;
+                
+                //Remove '.fa-'
+                filterOut = $scope.ReplaceAll(filterOut, '.fa-', '');
+                                
+                //Remove ':before {'
+                filterOut = $scope.ReplaceAll(filterOut, ':before {', '');
+                
+                //Remove every 2nd and 3rd line
+                var lines = filterOut.split("\n");
+                      
+                filterOut = '';
+                var inLineCounter = 0
+                for (var i = 0; i < lines.length; i++) {
+                    var line = lines[i];
+                    var textToAdd = null;
+                    
+                    // console.log(inLineCounter + ": " + line);
+
+                    if(line.length > 0){
+                        //Does the line end in a ,                    
+                        if(line.slice(-1) === ','){
+                            //Eg dedent,                            
+                            textToAdd = $scope.ReplaceAll(line, ':before,', '');
+                            
+                            //No inLineCounter++;        
+                        }                                                        
+                        else if(inLineCounter % 3 === 0){
+                            textToAdd = line;
+                            
+                            inLineCounter++;
+                        } else
+                            inLineCounter++;   
+                            
+                        if(textToAdd)
+                            filterOut += 'fontAwesomeOptions.push(\'' + textToAdd + '\');'+ '\n';
+                    }            
+                }
+                
+                $scope.FilterOut = filterOut;
+            };
             
             self.Initialise();
         }]);
